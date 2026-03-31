0ceddb4c4051536fe98efd8c071a39c30756b0df
jiuker <jiuker@minio.io>
2026-03-31 11:51:02 +0800

    fix: aws will not return parts anymore when do GetObjectAttributes for multi parts object
    fix: aws will not return parts anymore when do GetObjectAttributes for multi parts object

--- run/core/aws-sdk-go-v2/main.go
+++ run/core/aws-sdk-go-v2/main.go
@@ -1095,8 +1095,8 @@ func testGetObjectAttributesMultipart(ctx context.Context, s3Client *s3.Client) 
 		return
 	}
 
-	if attrResp.ObjectParts.Parts == nil || len(attrResp.ObjectParts.Parts) != 3 {
-		failureLog(function, args, startTime, "", fmt.Sprintf("AWS SDK Go V2 Parts array mismatch: expected 3 parts, got %d", len(attrResp.ObjectParts.Parts)), errors.New("parts array mismatch")).Fatal()
+	if attrResp.ObjectParts.TotalPartsCount != nil && *attrResp.ObjectParts.TotalPartsCount != 3 {
+		failureLog(function, args, startTime, "", fmt.Sprintf("AWS SDK Go V2 Parts array mismatch: expected 3 parts, got %d", *attrResp.ObjectParts.TotalPartsCount), errors.New("parts array mismatch")).Fatal()
 		return
 	}
 
